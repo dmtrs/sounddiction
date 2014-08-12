3c66fd238126c6f2334bddd50355990badd7728f
Dimitrios Mengidis <tydeas@peopleperhour.com>
2014-08-12 19:34:05 +0300

    ERR connect

--- config-example.js
+++ config-example.js
@@ -2,3 +2,7 @@ module.exports.soundcloud = {
     "id" : null,
     "secret": null
 }
+module.exports.predictionio = {
+    "key": null,
+    "baseUrl": 'http://127.0.0.1:8000'
+}
--- crawl.js
+++ crawl.js
@@ -1,8 +1,13 @@
 var config = require('./config');
+
 var when   = require('when');
 var guard  = require('when/guard');
+var nodefn = require('when/node');
+
 var SoundCloudAPI = require("soundcloud-node");
 var client = new SoundCloudAPI(config.soundcloud.id, config.soundcloud.secret, 'http://localhost:3000/callback');
+
+var prediction   = require('predictionio')(config.predictionio);
 
 var me = function(token) {
     var user = when.defer();
@@ -41,21 +46,23 @@ var result_set = me('1-56332-48830-d09c9efa96c76587d0').then(function(user) {
     return when.promise(function(resolve, reject) {
         user_favorites(user.id)
             .then(function(tracks) {
-                when.map(tracks, guard(guard.n(100), function(track, i) { return track_favoriters(track.id); }))
+                when.map(tracks, guard(guard.n(10), function(track, i) { return track_favoriters(track.id); }))
                     .then(function(users) {
                         var result_user_set = [];
-                        [].concat.apply([], users).forEach(function(user) {
+                        [].concat.apply([], users).forEach(guard(guard.n(10), function(user) {
                             if(result_user_set.indexOf(user.id)==-1) {
                                 result_user_set.push(user.id);
+                                var puser = when.defer();
+                                prediction.users.create({ pio_uid: user.id }, nodefn.createCallback(puser.resolver));
+                                puser.promise.then(function(res) {
+                                    console.log('user', user.id);
+                                }, function(err) {
+                                    console.log(err);
+                                });
                             }
                             resolve(result_user_set);
-                        });
+                        }));
                     });
             });
     });
-});
-
-
-result_set.then(function(users) {
-    console.log(users.length);
 });
